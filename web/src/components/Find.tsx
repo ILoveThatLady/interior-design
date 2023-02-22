0a9af352ec19cd65045d9f7a3664f3f1f7ebda2d
Emilio Ortiz <ortizemilio.pers@gmail.com>
2023-02-21 18:53:34 -0600

    header section 1/4

---
+++ web/src/components/Find.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Find.css'
+
+const Find = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Find
---
+++ web/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero--container'>
+      <div className='mask'></div>
+      <h1>I-Design</h1>
+      <p>From your mind<br/>to your home</p>
+    </div>
+  )
+}
+
+export default Hero
